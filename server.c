283fd77bb109949f54705fca11266e905ed9dad6
rokity <riccardo.amadio1@gmail.com>
2018-09-18 15:09:28 +0200

    server finito , facciamo dei test

--- server.c
+++ server.c
@@ -5,6 +5,59 @@
 #include <string.h>
 #include <unistd.h>
 
+
+int socket_client_mac;
+void initCLientSocket()
+{
+     int SERVER_PORT = 2001;
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(SERVER_PORT);
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    int listen_sock;
+    if ((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("could not create listen socket\n");
+    }
+    if ((bind(listen_sock, (struct sockaddr *)&server_address,
+              sizeof(server_address))) < 0)
+    {
+        printf("could not bind socket\n");
+    }
+    int wait_size = 1000;
+    if (listen(listen_sock, wait_size) < 0)
+    {
+        printf("could not open socket for listening\n");
+    }
+    struct sockaddr_in client_address;
+    int client_address_len = 0;
+    while (true)
+    {
+        // open a new socket to transmit data per connection
+        if ((socket_client_mac =
+                 accept(listen_sock, (struct sockaddr *)&client_address,
+                        &client_address_len)) < 0)
+        {
+            printf("could not open a socket to accept data\n");
+        }
+
+        printf("client connected with ip address: %s\n",
+               inet_ntoa(client_address.sin_addr));
+
+    }
+}
+
+
+
+void sendBufferToClientMac(char buffer[])
+{
+    size_t len = strlen(buffer);
+    send(socket_client_mac, buffer, len, 0);
+}
+
+
+
 void serverIphone()
 {
     int SERVER_PORT = 2000;
@@ -57,9 +110,9 @@ void serverIphone()
             len += n;
 
             printf("received: '%s'\n", buffer);
-
+            sendBufferToClientMac(buffer);
             // echo received content back
-            send(sock, buffer, len, 0);
+            
         }
 
         close(sock);
@@ -67,47 +120,6 @@ void serverIphone()
     close(listen_sock);
 }
 
-int socket_client_mac;
-void initCLientSocket()
-{
-     int SERVER_PORT = 2001;
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(SERVER_PORT);
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    int listen_sock;
-    if ((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("could not create listen socket\n");
-    }
-    if ((bind(listen_sock, (struct sockaddr *)&server_address,
-              sizeof(server_address))) < 0)
-    {
-        printf("could not bind socket\n");
-    }
-    int wait_size = 1000;
-    if (listen(listen_sock, wait_size) < 0)
-    {
-        printf("could not open socket for listening\n");
-    }
-    struct sockaddr_in client_address;
-    int client_address_len = 0;
-    while (true)
-    {
-        // open a new socket to transmit data per connection
-        if ((socket_client_mac =
-                 accept(listen_sock, (struct sockaddr *)&client_address,
-                        &client_address_len)) < 0)
-        {
-            printf("could not open a socket to accept data\n");
-        }
-
-        printf("client connected with ip address: %s\n",
-               inet_ntoa(client_address.sin_addr));
-
-    }
-}
 
 
 
---
+++ test_client_iphone.c
@@ -0,0 +1,12 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  
+
+}
