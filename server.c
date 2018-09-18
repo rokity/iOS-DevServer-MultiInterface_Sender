c922f4bd2a6864ad72df979a391e90f7a2b658da
rokity <riccardo.amadio1@gmail.com>
2018-09-18 12:53:24 +0200

    thread created and add server executable to gitignore

--- server.c
+++ server.c
@@ -4,8 +4,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 
+void *serverThreadIphone(void *vargp) 
+{ 
+    sleep(1); 
+    printf("Printing GeeksQuiz from Thread \n"); 
+    return NULL; 
+} 
 
 int main(int argc, char *argv[]) {
-    printf("ciao");
+    pthread_t serverThreadIphoneId; 
+    pthread_create(&serverThreadIphoneId, NULL, serverThreadIphone, NULL); 
+    
+    while(true)
+    {
+
+    }
 }
