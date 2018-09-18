1494a6bfd2d2ae21755e95b9286b05afdc7c23b5
rokity <riccardo.amadio1@gmail.com>
2018-09-18 12:29:56 +0200

    rename gitignore

---
+++ server.c
@@ -0,0 +1,11 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+    printf("ciao");
+}
