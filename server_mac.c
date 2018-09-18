970c4d3ff310b4944e3a401a2d818f4d8230e009
rokity <riccardo.amadio1@gmail.com>
2018-09-19 00:35:18 +0200

    rename server file

---
+++ server_mac.c
@@ -0,0 +1,88 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void listenFromServer()
+{
+    const char *server_name = "192.168.1.103";
+    const int server_port = 2001;
+
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+
+    // creates binary representation of server name
+    // and stores it as sin_addr
+    // http://beej.us/guide/bgnet/output/html/multipage/inet_ntopman.html
+    inet_pton(AF_INET, server_name, &server_address.sin_addr);
+
+    // htons: port in network order format
+    server_address.sin_port = htons(server_port);
+
+    // open a stream socket
+    int sock;
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("could not create socket\n");
+         
+    }
+
+    // TCP is connection oriented, a reliable connection
+    // **must** be established before any data is exchanged
+    if (connect(sock, (struct sockaddr *)&server_address,
+                sizeof(server_address)) < 0)
+    {
+        printf("could not connect to server\n");
+         
+    }
+
+    // receive
+
+    int n = 0;
+    int count = 0, maxlen = 1;
+    char buffer[maxlen];
+    char content[60030];
+
+    FILE *write_ptr;
+    write_ptr = fopen("foto.jpg", "wb");
+    
+    // will remain open until the server terminates the connection
+    while ((n = recv(sock, buffer, maxlen, 0)) > 0)
+    {
+        content[count]= buffer[0];
+        count++;
+        printf("received: '%s'\n", buffer);
+    }
+    if (n == 0)
+        {
+            puts("Client disconnected");
+            printf("%s", content);
+            char str[12];
+            sprintf(str, "%d", count);
+            puts(str);
+            // w for write, b for binary
+
+            fwrite(content, sizeof(content), 1, write_ptr);
+            fclose(write_ptr);
+            puts("file scritto");
+            // puts(count);
+            //fflush(stdin);
+        }
+        else if (n == -1)
+        {
+            perror("recv failed");
+        }
+
+}
+
+int main(int argc, char *argv[])
+{
+    listenFromServer();
+    while (true)
+    {
+    }
+}
--- server_sim.c
+++ server_sim.c
@@ -5,10 +5,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <pthread.h> 
+
 
 int socket_client_mac;
+
 void initCLientSocket()
 {
+    printf("true");
     int SERVER_PORT = 2001;
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
@@ -16,16 +22,19 @@ void initCLientSocket()
     server_address.sin_port = htons(SERVER_PORT);
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     int listen_sock;
+    printf("true");
     if ((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("could not create listen socket\n");
     }
+    printf("true");
     if ((bind(listen_sock, (struct sockaddr *)&server_address,
               sizeof(server_address))) < 0)
     {
         printf("could not bind socket\n");
     }
-    int wait_size = 1000;
+    printf("true");
+    int wait_size = 100000;
     if (listen(listen_sock, wait_size) < 0)
     {
         printf("could not open socket for listening\n");
@@ -34,6 +43,7 @@ void initCLientSocket()
     int client_address_len = 0;
     while (true)
     {
+        printf("true");
         // open a new socket to transmit data per connection
         if ((socket_client_mac =
                  accept(listen_sock, (struct sockaddr *)&client_address,
@@ -50,10 +60,10 @@ void initCLientSocket()
 void sendBufferToClientMac(char buffer[])
 {
     size_t len = strlen(buffer);
-    send(socket_client_mac, buffer, len, 0);
+    send(socket_client_mac, buffer, 1, 0);
 }
 
-void serverIphone()
+void *serverIphone(void *vargp)
 {
     int SERVER_PORT = 2000;
     struct sockaddr_in server_address;
@@ -95,32 +105,26 @@ void serverIphone()
 
         printf("client connected with ip address: %s\n",
                inet_ntoa(client_address.sin_addr));
-        int count = 0;
-        FILE *write_ptr;
-
-        write_ptr = fopen("foto.jpg", "wb");
-        char content[60030];
+       
+        
         // keep running as long as the client keeps the connection open
         while ((n = recv(sock, buffer, maxlen, 0)))
         {
-            content[count]= buffer[0];
-            printf("%s", buffer);
-            printf("\n");
-            count++;
+            sendBufferToClientMac(buffer);
             //printf("received: '%s'\n", buffer);
         }
         if (n == 0)
         {
             puts("Client disconnected");
-            printf("%s", content);
-            char str[12];
-            sprintf(str, "%d", count);
-            puts(str);
-            // w for write, b for binary
+            // printf("%s", content);
+            // char str[12];
+            // sprintf(str, "%d", count);
+            // puts(str);
+            // // w for write, b for binary
 
-            fwrite(content, sizeof(content), 1, write_ptr);
-            fclose(write_ptr);
-            puts("file scritto");
+            // fwrite(content, sizeof(content), 1, write_ptr);
+            // fclose(write_ptr);
+            // puts("file scritto");
             // puts(count);
             //fflush(stdin);
         }
@@ -134,11 +138,14 @@ void serverIphone()
         //close(sock);
     }
     //close(listen_sock);
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    serverIphone();
+    pthread_t thread_server_iphone; 
+    pthread_create(&thread_server_iphone, NULL, serverIphone, NULL);
+    printf("ciao");
     initCLientSocket();
     while (true)
     {
